a1b6709d0a3c51ac0c568b341909bce400d88c92
Nikolas, Vardeberg <nikolas.vardeberg1@gmail.com>
2025-05-26 18:23:04 +0200

    feat: companion card companions list and cta component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,48 @@
+import CompanionCard from "@/components/companion-card";
+import CompanionsList from "@/components/companions-list";
+import Cta from "@/components/cta";
+import { recentSessions } from "@/constants";
+
 export default function Home() {
   return(
-    <div>
-      hei
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      
+      <section className="home-section">
+        <CompanionCard 
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="science"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          className="w-2/3 max-lg:w-full"
+        />
+        <Cta />
+      </section>
+    </main>
   )
 }
