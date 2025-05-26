c820048e14582f4403dcca3f2481b7f060391f24
Nikolas, Vardeberg <nikolas.vardeberg1@gmail.com>
2025-05-26 20:05:50 +0200

    feat: search params and filters companion library

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,7 +1,36 @@
-export default function Page() {
+import CompanionCard from "@/components/companion-card";
+import SubjectFilters from "@/components/search-filters";
+import SearchInput from "@/components/search-input";
+import { getCompanions } from "@/lib/companion/actions";
+import { getSubjectColor } from "@/lib/utils";
+
+export default async function Page( { searchParams }: SearchParams) {
+    const filters = await searchParams; 
+    const subject = filters.subject ? filters.subject : "";
+    const topic = filters.topic ? filters.topic : "";
+
+    const companions = await getCompanions({ subject, topic });
+
+    console.log(companions)
+
     return(
-        <div>
-            
-        </div>
+        <main>
+            <section className="flex justify-between gap-4 max-sm:flex-col">
+                <h1>Companion Library</h1>
+                <div className="flex gap-4">
+                    <SearchInput />
+                    <SubjectFilters />
+                </div>
+            </section>
+            <section className="companions-grid">
+                {companions.map((companion) => (
+                    <CompanionCard 
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
+            </section>
+        </main>
     )
 }
