4814d6caa43c0d1ba339988c3c2f0ba9e3c5b4cd
Nikolas, Vardeberg <nikolas.vardeberg1@gmail.com>
2025-05-27 09:29:00 +0200

    feat: addToSessionHistory + getRecentSessions

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,31 @@
 import CompanionCard from "@/components/companion-card";
 import CompanionsList from "@/components/companions-list";
 import Cta from "@/components/cta";
-import { recentSessions } from "@/constants";
+import { getCompanions, getRecentSessions } from "@/lib/companion/actions";
+import { getSubjectColor } from "@/lib/utils";
 
-export default function Home() {
+export default async function Home() {
+  const companions = await getCompanions({ limit: 3});
+  const recentSessionsCompanions = await getRecentSessions(10);
+
   return(
     <main>
       <h1 className="text-2xl underline">Popular Companions</h1>
       
       <section className="home-section">
-        <CompanionCard 
-          id="123"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the brain"
-          subject="science"
-          duration={45}
-          color="#ffda6e"
-        />
-        <CompanionCard
-          id="456"
-          name="Countsy the Number Wizard"
-          topic="Derivatives & Integrals"
-          subject="science"
-          duration={30}
-          color="#e5d0ff"
-        />
-        <CompanionCard
-          id="789"
-          name="Verba the Vocabulary Builder"
-          topic="language"
-          subject="English Literature"
-          duration={30}
-          color="#BDE7FF"
-        />
+        {companions.map((companion) => (
+          <CompanionCard 
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
       </section>
 
       <section className="home-section">
         <CompanionsList
           title="Recently completed sessions"
-          companions={recentSessions}
+          companions={recentSessionsCompanions}
           className="w-2/3 max-lg:w-full"
         />
         <Cta />
